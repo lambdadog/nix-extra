4d30718c65084a8e8f970b3f39397e0f932188d9
Ashlynn Anderson <pea@pea.sh>
2020-05-01 20:38:41 -0400

    Beef up switch-to-environment

--- pkgs/user-environment/default.nix
+++ pkgs/user-environment/default.nix
@@ -8,6 +8,57 @@ let
     
     paths = packages;
   };
+
+  shellCommand = ''
+  nix-shell -p ${profile}
+  '';
+
+  testCommand = ''
+  nix-env --set ${profile}
+  '';
+
+  switchCommand = ''
+  echo "Switching to read-only profile"
+  nix-env --switch-profile ${profile}
+  '';
+
+  resetCommand = ''
+  if [ -d "/nix/var/nix/profiles/per-user/$USER/profile/" ]; then
+    echo "Resetting profile to /nix/var/nix/profiles/per-user/$USER/profile/"
+    nix-env --switch-profile "/nix/var/nix/profiles/per-user/$USER/profile/"
+  else
+    echo "Couldn't find a per-user profile for $USER"
+    echo "Resetting profile to /nix/var/nix/profiles/default/"
+    nix-env --switch-profile "/nix/var/nix/profiles/default/"
+  fi
+  '';
+
+  commandHelp = ''
+  echo "$0 [shell|test|switch|reset]"
+  '';
+
 in writeShellScriptBin "switch-to-environment" ''
-nix-env --set ${profile}
+case $1 in
+
+"shell")
+${shellCommand}
+;;
+
+"test")
+${testCommand}
+;;
+
+"switch")
+${switchCommand}
+;;
+
+"reset")
+${resetCommand}
+;;
+
+*)
+${commandHelp}
+;;
+
+esac
 ''
